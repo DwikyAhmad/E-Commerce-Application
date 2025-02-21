2580dc2d44766cf43de287f6f51f5c6beb25c05a
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-02-21 16:26:29 +0700

    feat: add feature all wishlist to cart

--- ECommerceApplication/src/main/java/com/app/controllers/WishlistController.java
+++ ECommerceApplication/src/main/java/com/app/controllers/WishlistController.java
@@ -2,6 +2,7 @@ package com.app.controllers;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.entites.Cart;
+import com.app.entites.Product;
 import com.app.payloads.WishlistDTO;
 import com.app.services.WishlistService;
+import com.app.services.CartService;
+import com.app.payloads.CartDTO;
+import com.app.payloads.ProductDTO;
+import com.app.payloads.ResponseDTO;
+import com.app.entites.Wishlist;
+import com.app.entites.User;
+import com.app.services.UserService;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
@@ -24,6 +34,13 @@ public class WishlistController {
 
     @Autowired
     private WishlistService wishlistService;
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private UserService userService;
+
 
     // Add a product to a user's wishlist. Note that wishlist items don't require quantity.
     @PostMapping("/public/wishlists/{wishlistId}/products/{productId}")
@@ -59,5 +76,24 @@ public class WishlistController {
             @PathVariable Long productId) {
         String status = wishlistService.deleteProductFromWishlist(wishlistId, productId);
         return new ResponseEntity<>(status, HttpStatus.OK);
+    }
+
+    // Add all items from wishlist to cart and clear the wishlist
+    @PostMapping("/public/{email}/wishlists/{wishlistId}/moveToCart")
+    public ResponseEntity<?> addWishlistItemsToCart(@PathVariable String email, @PathVariable Long wishlistId) {
+        List<ProductDTO> products = wishlistService.getWishlist(wishlistId).getProducts();
+
+
+        for (ProductDTO product : products) {
+            cartService.addProductToCart(wishlistId, product.getProductId(), 1);
+            wishlistService.deleteProductFromWishlist(wishlistId, product.getProductId());
+        }
+
+        CartDTO cart = cartService.getCart(email, wishlistId);
+
+        String message = "All items from wishlist moved to cart";
+        ResponseDTO response = new ResponseDTO(message, cart, 200);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
